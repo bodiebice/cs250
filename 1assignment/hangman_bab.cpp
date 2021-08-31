8eb5f95666a75ce5843b5e73b868e59f7703dbce
Bodie Bice <bodiebice@DESKTOP-5C0IJ7S.localdomain>
2021-08-30 23:55:54 -0500

    First draft of HW1.

---
+++ 1assignment/hangman_bab.cpp
@@ -0,0 +1,41 @@
+//this code allows for the user to play hangman against a computer with a set program.
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int checkplease(char[5], char &);
+int verifywinner(char[5], char &, int &,);
+int checkloss
+int main()
+{
+  char word[5] = {'t','i','t','a','n'};
+  char guessed;
+  bool won = false; 
+  int bingo = 0;
+  //while (won==false)
+  //{
+    cout<<"Enter a character to guess for Hangman\n";
+    cin>>guessed;
+
+    bingo = checkplease(word, guessed);
+    cout<<bingo<<"\n";
+  //}
+  return 0;
+}
+
+int checkplease(char word[5], char &guessed)
+{
+  int index = 0;
+  int bingo = -1;
+
+  for (index=0;index<=5;index++){
+    if (guessed==word[index]){
+      bingo=1;
+    }
+    
+  }
+  index=0;
+  return bingo;
+}
+
