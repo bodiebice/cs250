e918c7ff29877cac595ee949fe3b259e32b7e2d0
BodieBice <bbicelaptop@LAPTOP-U6HL3OK6.localdomain>
2021-09-03 23:04:18 -0500

    Hopefully Final Version!

--- 1assignment/hangman_bab.cpp
+++ 1assignment/hangman_bab.cpp
@@ -4,30 +4,52 @@
 #include <iomanip>
 using namespace std;
 
-int checkplease(char[5], char &);
-int verifywinner(char[5], char &);
-int countloss(char[5], char &);
+int checkPlease(char[5], char &);
+int verifyWinner(char[5], char &);
+int countLoss(char[5], char &);
 int main()
 {
   char word[5] = {'v','e','n','u','s'};
+
   char guessed[5]={'*','*','*','*','*'};
   char guess;
   bool game = true; 
-  int bingo = 0;
+  int bingo = -1;
+  int corr = 0;
+  int incor = 0;
+  int attleft = 5;
+
+    // this while loop will send the user through picking a letter to guess and then 
+    // it will determine at the end if the game is over, if it is through the two conditions,
+    // it will set bool *game* to true.
+
   while (game==true)
   {
-    cout<<"Enter a character to guess for Hangman\n";
+    cout<<"\nEnter a character to guess for Hangman (undercase letters only)\n";
     cin>>guess;
 
-    bingo = checkplease(word, guess);
+    bingo = checkPlease(word, guess);
+
+    // depending on the input code from the function checkPlease, it will send the variable guess along
+    // with the respective array to either verifyWinner in case of a hit, and countLoss in case of a miss
+
     switch (bingo) {
-      case 1: bingo = verifywinner(word, guess);
+      case 1: 
+        corr++;
+        cout<<"Correct! You have correctly guessed "<<corr<<" letters correct out of 5! \n";
+        bingo = verifyWinner(word, guess);
       break;
-      case -1: bingo = countloss(guessed, guess);
+      case -1:
+        incor++;
+        attleft = attleft - incor;
+        cout<<"Incorrect, you have "<<attleft<<" attempts left! \n";
+        bingo = countLoss(guessed, guess);
+        attleft = 5;
       break;
     }
-    cout<<word<<guessed<<'\n';
-    
+
+    // this section of while loop determines if either switch case function 
+    // gave a code that signals that the user either won or lost.
     if (bingo==2){
       game = false;
       cout<<"Congrats, You've won!\n";
@@ -42,13 +64,14 @@ int main()
   }
   return 0;
 }
-//this function checks the guessed character against the word being guessed.
-int checkplease(char word[5], char &guess)
+    // this function checks the guessed character against the word being guessed.
+
+int checkPlease(char word[5], char &guess)
 {
   int index = 0;
   int bingo = -1;
-  //go through the word venus to see if the word guessed is in the array
-  for (index=0;index<=5;index++){
+    // go through the word venus to see if the word guessed is in the array
+  for (index=0;index<=4;index++){
     if (guess==word[index]){
       bingo=1;
       word[index] = '*';
@@ -58,8 +81,10 @@ int checkplease(char word[5], char &guess)
   return bingo;
 }
 
-//this function keeps track of what characters have been guessed correctly and accurately tells the computer when the word has been guessed.
-int verifywinner(char word[5], char &guess)
+    // this function keeps track of what characters have been guessed correctly 
+    // and accurately tells the computer when the word has been guessed.
+
+int verifyWinner(char word[5], char &guess)
 {
   int bingo = 1;
   if (word[4]=='*'){
@@ -67,16 +92,24 @@ int verifywinner(char word[5], char &guess)
   }
   return bingo;
 }
-// this function will keep trach of incorrect characters guessed and will detect when a user looses
-int countloss(char guessed[5], char&guess)
+    // this function will keep trach of incorrect characters guessed and will detect when a user looses
+
+int countLoss(char guessed[5], char&guess)
 {
   int index = 0;
   int bingo = -1;
-  guessed[index] = guess;
+  bool gotcha = false;
+  gotcha = false;
+  for (index =0; index<=4; index++)
+  { 
+    if (guessed[index]=='*'&&gotcha==false){
+      guessed[index]=guess;
+      gotcha=true;
+    }
+  }
   if (guessed[4]!='*'){
     bingo=3;
   }
-  index++;
+  ;
   return bingo;
 }
-
