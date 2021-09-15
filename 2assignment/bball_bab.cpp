3fae572db071eba0a37e24cd7af2e7ff8179e96d
Bodie Bice <bodiebice@DESKTOP-5C0IJ7S.localdomain>
2021-09-15 12:56:25 -0500

    I think this is the release candidate!

--- 2assignment/bball_bab.cpp
+++ 2assignment/bball_bab.cpp
@@ -5,45 +5,50 @@ using namespace std;
 const int row_length = 10;
 const int col_length = 7;
 
-void dataIn(double[row_length][col_length]);
-void dataOut(double[col_length], double [row_length], double&);
-void averageGame(double[row_length][col_length], double [col_length]);
-void averagePlayer(double[row_length][col_length], double[row_length]);
-void averagePPG(double[row_length][col_length], double&);
+void dataIn(float[row_length][col_length]);
+void dataOut(float[col_length], float [row_length], float&);
+void averageGame(float[row_length][col_length], float [col_length]);
+void averagePlayer(float[row_length][col_length], float[row_length]);
+void averagePPG(float[row_length][col_length], float&);
 
 int main ()
 {
-  double stats[row_length][col_length];
-  double ppg[7];
-  double ppp[10];
-  double appg = 0;
-  int test = 0;
+  float stats[row_length][col_length];
+  float ppg[7];
+  float ppp[10];
+  float appg = 0;
+  float test = 0;
   cout<<"Welcome to my program."<<endl;
   dataIn(stats);
-  //averageGame(stats, ppg);
-  //averagePlayer(stats, ppp);
-  //dataOut(ppg,ppp,appg);
+  averageGame(stats, ppg);
+  averagePlayer(stats, ppp);
+  averagePPG(stats, appg);
+  dataOut(ppg,ppp,appg);
   
   
 
   return 0;
 }
 
-void dataIn(double stats[row_length][col_length])
+void dataIn(float stats[row_length][col_length])
 {
   ifstream inputFile;
   inputFile.open("game_data.txt");
-  int index = 0;
-  int indexy = 0;
-  int placeholdervalue;
-  // this following double for loop will read in the data of a file and put it into a 2d array.
+  int index = 0, indexy = 0, indexo = 0, count  = 0;
+  float intake[70];
+  for (indexo=0; indexo<70; indexo++)
+  {
+    inputFile >> intake[indexo];
+    //cout << intake[indexo] << " " << indexo <<endl;
+  }
+  // this following float for loop will read in the data of a file and put it into a 2d array.
   for (index = 0; index<row_length; index++)
   {
     for (indexy = 0; indexy<col_length; indexy++)
     {
-      inputFile >> placeholdervalue;
-      stats [index][indexy] = placeholdervalue;
-      cout << stats[index][indexy]<<index<<indexy<<endl;
+      stats [index][indexy] = intake[count];
+      count++;
+      //cout << stats[index][indexy]<<" "<<index<<indexy<<endl;
       
     }
   inputFile.close();
@@ -51,12 +56,12 @@ void dataIn(double stats[row_length][col_length])
   return;
 }
 
-void averageGame(double stats[row_length][col_length], double ppg[col_length])
+void averageGame(float stats[row_length][col_length], float ppg[col_length])
 {
   int index = 0;
   int indexy = 0;
-  double tally = 0;
-  double counter = 0;
+  float tally = 0, counter = 0;
+  
   for (index = 0; index<col_length; index++)
   {
     tally = 0;
@@ -64,25 +69,26 @@ void averageGame(double stats[row_length][col_length], double ppg[col_length])
     counter = 0;
     for (indexy = 0; indexy<row_length; indexy++)
     {
-      cout<<stats[indexy][index]<<" ";
+      
       tally +=stats[indexy][index];
       counter++;
       
       // the code below was my test when debugging to make sure that tally was being added up correctly.
       //cout<<tally<<endl;
     }
-    cout<<endl<<tally<<endl;
+    
     ppg[index] = tally/counter;
+    
     //the code below was my test to make sure that the index was being put into points per game correctly.
     //cout<<ppg[index]<<endl;
   }
   return;
 }
 
-void averagePlayer(double stats[row_length][col_length], double ppp[row_length])
+void averagePlayer(float stats[row_length][col_length], float ppp[row_length])
 {
   int index = 0, indexy = 0;
-  double tally = 0, counter = 0;
+  float tally = 0, counter = 0;
   for (index = 0; index<row_length; index++)
   {
     tally = 0;
@@ -97,10 +103,10 @@ void averagePlayer(double stats[row_length][col_length], double ppp[row_length])
   return;
 }
 
-void averagePPG(double stats[row_length][col_length], double &appg)
+void averagePPG(float stats[row_length][col_length], float &appg)
 {
   int index = 0, indexy = 0;
-  double tally = 0, counter = 0;
+  float tally = 0, counter = 0;
   for (index = 0; index<row_length; index++)
   {
     for (indexy = 0; indexy<col_length; indexy++)
@@ -108,15 +114,15 @@ void averagePPG(double stats[row_length][col_length], double &appg)
       tally += stats[index][indexy];
       counter++;
     }
-    cout<<tally<<endl;
+    //cout<<tally<<endl;
   }
   appg = tally/counter;
-  cout<<appg<<endl;
+  //1cout<<appg<<endl;
 
   return;
 }
 
-void dataOut(double ppg [col_length], double ppp [row_length], double &appg)
+void dataOut(float ppg [col_length], float ppp [row_length], float &appg)
 {
   int menu = -1;
   int index = 0;
@@ -134,7 +140,7 @@ void dataOut(double ppg [col_length], double ppp [row_length], double &appg)
         for (index = 0; index<col_length; index++)
         {
           gamecount++;
-          cout<<"Game"<<gamecount<<": "<<ppg[index]<<endl;
+          cout<<"Game "<<gamecount<<": "<<ppg[index]<<endl;
         }
       break;
       case 2: 
@@ -143,17 +149,17 @@ void dataOut(double ppg [col_length], double ppp [row_length], double &appg)
         for (index  = 0; index<row_length; index++)
         {
           gamecount++;
-          cout<<"Player"<<gamecount<<": "<<ppp[index]<<endl;
+          cout<<"Player "<<gamecount<<": "<<ppp[index]<<endl;
         }
       break;
       case 3:
-        cout<<"The average points scored per player per game was"<<appg<<endl;
+        cout<<"The average points scored per player per game was "<<appg<<endl;
       break;
       case 4:
         done = true;
       break;
       default: 
-        cout<<"Sorry, the integer you entered was not valid, enter a valid intiger."<<endl;
+        cout<<"Sorry, the integer you entered was not valid, enter a valid integer."<<endl;
       break;
     }
   }
