5a0214ac89dc42534c6a7192ed5fefbc0b189459
Bodie Bice <bodiebice@Bodies-MacBook-Air.local>
2021-09-21 10:56:08 -0500

    hi

--- 3assignment/bank_bab.cpp
+++ 3assignment/bank_bab.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <istream>
 using namespace std;
 
 // this col_width var will be however long the account numbers for this institution will be, assuming that they're constant.
 const int col_width = 7;
-int rows = 0;
+
 
 void welcome(int&);
 int arraySize();
-void dataIn(int[][col_width]);
-void dataSort(int[][col_width]);
-void dataSearch(int[][col_width]);
-void dataPrint(int&,int&);
+void dataIn(int[][col_width], int&);
+//void dataSort(int[][col_width]);
+//void dataSearch(int[][col_width]);
+//void dataPrint(int&,int&);
 
 int main()
 {
   int initialize = 0;
   welcome(initialize);
-  if (initialize==true)
+  if (initialize==1)
   {
-    const int rows = arraySize();
+    int rows = arraySize();
     int datamain[rows][col_width];
-    dataIn(datamain);
-    dataSort(datamain);
-    dataSearch(datamain);
+    dataIn(datamain,rows);
+    //dataSort(datamain);
+    //dataSearch(datamain);
   }
 
   return 0;
 }
 
+// ****************************************************************** //
+// this function welcomes the user to the program and will tell them  //
+// what the requirements to use the program are before they continue  //
+// to make sure that the user knows what they need for the program.   //
+// ****************************************************************** //
+
 void welcome(int &initialize)
 {
+  cout<<endl;
   cout<< "Welcome to this Program! To Continue, you must have a txt file named accounts.txt"<<endl;
   cout<< "and within that file the first line should have the number of accounts in the dataset."<<endl;
   cout<<endl<<"If you acknowledge this, Press 1, press any other character to exit the program."<<endl;
@@ -44,3 +52,53 @@ void welcome(int &initialize)
     }
   return;
 }
+
+// ******************************************************************   //
+// this function will take the integer in accounts.txt that determines  //
+// how many rows of data the 2d array will need to hold account numbers //
+// and inputs that into a global constant varaible titled 'rows'        //
+// ******************************************************************   //
+
+int arraySize()
+{
+  int rows = 0;
+  ifstream inputFile;
+  inputFile.open("accounts.txt");
+  inputFile>>rows;
+  cout<<rows<<endl;
+  
+
+  return rows;
+
+}
+
+
+void dataIn(int datamain[][col_width], int& rows)
+{
+  cout<<"Hi World"<<endl;
+  ifstream inputFile;
+  int placeholder, index = 0, indexio = 0, indexia = 0, counter = 0;
+  int holddata[10000];
+  inputFile.open("accounts.txt");
+  for (indexia = 0; indexia>10000; indexia++)
+  {
+    inputFile>>placeholder;
+    cout<<placeholder;
+    if (placeholder < 0)
+    {
+      holddata[indexia] = placeholder;
+    }
+    cout<<holddata[indexia]<<" ";
+  }
+  for (index = 0; index<rows; index++)
+  {
+    for (indexio = 0; indexio<col_width; indexio++)
+    {
+      datamain[index][indexio] = holddata[counter];
+      counter++;
+    }
+  }
+  
+  return;
+}
+
