83ed28d671f52532f30a2f0ae6edbfb2167a9eef
Bodie Bice <bodiebice@Bodies-MacBook-Air.local>
2021-09-26 19:05:55 -0500

    ya

--- 3assignment/bank_bab.cpp
+++ 3assignment/bank_bab.cpp
@@ -8,19 +8,23 @@ using namespace std;
 // this col_width var will be however long the account numbers for this institution will be, assuming that they're constant.
 static int rows = 0;
 int* rptr = &rows;
+static bool done = false;
 
-void welcome(int&);
+void welcome(char*);
 void arraySize(int*);
 void dataIn(int*, int*);
 void dataSort(int*);
-//void dataSearch(int[]);
-//void dataPrint(int&,int&);
+void dataSwap(int*, int*);
+void dataQuery(int*);
+void dataSearch(int*, int*, int*, int*);
+void dataPrint(int*, int*);
 
 int main()
 {
-  int initialize = 0;
-  welcome(initialize);
-  if (initialize==1)
+  char initialize = 0;
+  char* cptr = &initialize;
+  welcome(cptr);
+  if (initialize=='1')
   {
     arraySize(rptr);
     int datamain[*rptr];
@@ -28,7 +32,12 @@ int main()
 
     dataIn(rptr, dptr);
     dataSort(dptr);
-    //dataSearch(datamain);
+    //  below is my extremely lazy check for if the inital sort function worked on the data provided for the test.
+    //  cout<<dptr[0]<<endl<<dptr[1]<<endl<<dptr[2]<<endl<<dptr[3]<<endl<<dptr[4]<<endl;
+    if (done == false)
+      {
+        dataQuery(dptr);
+      }
   }
 
   return 0;
@@ -40,18 +49,19 @@ int main()
 // to make sure that the user knows what they need for the program.   //
 // ****************************************************************** //
 
-void welcome(int &initialize)
+void welcome(char *cptr)
 {
   cout<<endl;
   cout<< "Welcome to this Program! To Continue, you must have a txt file named accounts.txt"<<endl;
   cout<< "and within that file the first line should have the number of accounts in the dataset."<<endl;
   cout<<endl<<"If you acknowledge this, Press 1, press any other character to exit the program."<<endl;
-  cin>>initialize;
+  cin>>*cptr;
   while (cin.fail() == true)
     {
+      cout<<"invalid character"<<endl;
       cin.clear();
       cin.ignore(std::numeric_limits<int>::max(),'\n');
-      cin>>initialize;
+      cin>>*cptr;
     }
   return;
 }
@@ -67,12 +77,12 @@ void arraySize(int *rptr)
   int rows = 0;
   ifstream inputFile;
   inputFile.open("accounts.txt");
+  // puts the first integer that designates how big the array is into a pointer back to rows, a global static.
   inputFile>>*rptr;
-  cout<<*rptr<<endl;
+  // below is my check to make sure the thing works.
+  // cout<<*rptr<<endl;
   inputFile.close();
-
   return;
-
 }
 
 // ********************************************************************   //
@@ -83,25 +93,131 @@ void arraySize(int *rptr)
 
 void dataIn(int *rptr, int *dptr)
 {
-  
-  cout<<"Hi World"<<endl;
   ifstream inputFile;
-  int placeholder, index = 0, indexio = 0,counter = 0;
-  int holddata[rows];
+  int placeholder, index = 0, garbagevalue = 0, trashvalue = 0;;
   inputFile.open("accounts.txt");
-  inputFile>>counter;
-  //this for loop is to 
+  // this inputFile will take the number that designates how big the array is into a garbage integer
+  inputFile>>garbagevalue;
+  // this for loop is to put the rest of the input into the array in main through pointers
   for (index = 0; index<rows; index++)
   {
-    inputFile>>dptr[index];
-    cout<<dptr[index]<<endl;
-    
+    inputFile>>trashvalue;
+    if (trashvalue>0)
+    {
+      dptr[index] = trashvalue;
+    }
+    //cout<<dptr[index]<<endl;
   }
   return;
 }
 
 void dataSort(int *dptr)
 {
+  int minindex, minvalue;
+  for (int start = 0; start<(rows-1);start++)
+  {
+    minindex = start;
+    minvalue = dptr[start];
+    for (int index = start + 1; index < rows; index++)
+      {
+        if (dptr[index] < minvalue)
+          {
+            minvalue = dptr[index];
+            minindex = index;
+          }
+    
+    }
+    int* a = &dptr[minindex];
+    int* b = &dptr[start];
+
+    dataSwap(a,b);
+  
+  }
+  return;
+}
+
+void dataSwap(int* a, int* b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+  return;
+}
+
+void dataQuery(int* dptr)
+{
+  int searched = 0;
+  int* sptr = &searched;
+  cout<<endl<<endl<<"Enter an account number you'd like to search. Enter -1 if you'd like to exit the program."<<endl;
+  cin>>*sptr;
+  while (cin.fail() == true)
+    {
+      cout<<"invalid input, please enter an integer of the account number: "<<endl;
+      cin.clear();
+      cin.ignore(std::numeric_limits<int>::max(),'\n');
+      cin>>*sptr;
+    }
+  if (*sptr==-1)
+  {
+    done = true;
+    cout<<"Program is Closing, Have a Good Day!"<<endl;
+  }
+  int flagio;
+  int* fptr = &flagio;
+  dataSearch(dptr,rptr,sptr,fptr);
+  dataPrint(sptr, fptr);
+  
   
   return;
+}
+
+void datasearch(int* dptr,int* rptr, int* sptr, int* fptr)
+
+{
+  int first = 0;
+  int last = *rptr - 1;
+  int middle;
+  int position = -1;
+  bool found = false;
+
+  while (!found && first<=last)
+  {
+    middle = (first+last)/2;
+    if (dptr[middle]== *sptr)
+    {
+      found = true;
+      position = middle;
+    }
+    else if(dptr[middle]>middle)
+    {
+      last = middle-1;
+    }
+    else
+    {
+      first = middle+1;
+    }
+  }
+
+  if (found==true)
+  {
+    *fptr = 1;
+  }
+  else if (found==false)
+  {
+    *fptr = 2;
+  }
+
+  return;
+}
+
+void dataPrint(int* sptr, int* fptr)
+{
+  if (*fptr ==1)
+  {
+    cout<<"The number you entered is a valid account number"<<endl;
+  }
+  else
+  {
+    cout<<"The account number you entered is not in our system, try again?"<<endl;
+  }
 }
